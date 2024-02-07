ee9e5e1d2c2b61286bc6498efc12c5f3c784bfd6
ndellagrotte <100170700+ndellagrotte@users.noreply.github.com>
2024-02-07 01:13:30 -0500

    Update and rename main.py to py-chrome-eq-script.py
    - renamed it
    - added error checking for a bunch of stuff
    - useage now inolves passing the input file as a CLI argument
    - explicitly ignores preamp
    - auto sorting of filter types

---
+++ py-chrome-eq-script.py
@@ -0,0 +1,50 @@
+import json
+
+data = {
+    "test": {
+        "frequencies": [0] * 11,
+        "gains": [0] * 11,
+        "qs": [0] * 11
+    }
+}
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+pk_index = 1
+
+for i, line in enumerate(lines):
+    if line.startswith("Preamp:"):
+        continue
+
+    filter_params = line.split()
+    if "Filter" in filter_params:
+        fc_idx = filter_params.index("Hz") - 1
+        gain_idx = filter_params.index("Gain") + 1
+        q_idx = filter_params.index("Q") + 1
+
+        if "LSC" in filter_params:
+            data["test"]["frequencies"][0] = float(filter_params[fc_idx])
+            data["test"]["gains"][0] = float(filter_params[gain_idx])
+            data["test"]["qs"][0] = float(filter_params[q_idx])
+        elif "HSC" in filter_params:
+            data["test"]["frequencies"][10] = float(filter_params[fc_idx])
+            data["test"]["gains"][10] = float(filter_params[gain_idx])
+            data["test"]["qs"][10] = float(filter_params[q_idx])
+        elif "PK" in filter_params:
+            if pk_index > 9:
+                print("Error: Input EQ is limited to one HSC filter, one LSC filter, and 9 PK filters.")
+                print("Press return to exit")
+                input()
+                exit()
+            data["test"]["frequencies"][pk_index] = float(filter_params[fc_idx])
+            data["test"]["gains"][pk_index] = float(filter_params[gain_idx])
+            data["test"]["qs"][pk_index] = float(filter_params[q_idx])
+            pk_index += 1
+
+with open("eq-script-output.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+print("Success. Import `eq-script-output.json` into the Ears EQ extension and enjoy.")
+print("Press return to exit")
+input()
